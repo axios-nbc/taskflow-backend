f43b74c272a645ec9400dfe8679498c45fad8aa8
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-05 05:08:30 +0900

    feat: CORS 설정

---
+++ src/main/java/org/example/taskflowd/common/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.example.taskflowd.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")  // 프론트엔드 주소
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
