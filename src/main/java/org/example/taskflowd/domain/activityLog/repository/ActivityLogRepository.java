0a6e863111ab4d025a1c0e31bb280303b3d199fe
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-09-08 10:42:09 +0900

    feat: ActivityLogRepository에 쿼리 메서드 추가

--- src/main/java/org/example/taskflowd/domain/activityLog/repository/ActivityLogRepository.java
+++ src/main/java/org/example/taskflowd/domain/activityLog/repository/ActivityLogRepository.java
@@ -3,6 +3,9 @@ package org.example.taskflowd.domain.activityLog.repository;
 import org.example.taskflowd.domain.activityLog.entity.ActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long>, JpaSpecificationExecutor<ActivityLog> {
+    Page<ActivityLog> findByUser_IdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 }
