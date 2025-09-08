2c948fc39b2e4f25839ff1b56234384e87f2f27b
moominIsCute <1happymoomin1@gmail.com>
2025-09-08 16:19:40 +0900

    feat: 코멘트 제외 테스트 잘돌아감

--- src/test/java/org/example/taskflowd/doamin/comment/service/CommentExternalServiceTest.java
+++ src/test/java/org/example/taskflowd/doamin/comment/service/CommentExternalServiceTest.java
@@ -32,73 +32,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(MockitoExtension.class)
 public class CommentExternalServiceTest {
 
-//    @InjectMocks
-//    private CommentExternalService commentExternalService;
-//
-//    @Mock
-//    private CommentRepository commentRepository;
-//    @Mock
-//    private UserRepository userRepository;
-//    @Mock
-//    private TaskRepository taskRepository;
-//
-//    @Test
-//    @DisplayName("[CommentService - createdComment] 정상 작동 확인")
-//    @Transactional
-//    void createComment() {
-//
-//        // given
-//        CreateCommentRequest createCommentRequest = new CreateCommentRequest("내용", null);
-//        User user = new User();
-//        Task task = new Task("title", "description", user, user, TaskStatus.TODO, TaskPriority.HIGH, LocalDateTime.now());
-//        ReflectionTestUtils.setField(user, "id", 1L);
-//        ReflectionTestUtils.setField(task, "id", 2L);
-//        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task));
-//        given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
-//
-//        Comment savedComment = Comment.create(createCommentRequest, task, user);
-//        ReflectionTestUtils.setField(savedComment, "id", 1L);
-//        given(commentRepository.save(any(Comment.class))).willReturn(savedComment);
-//
-//        // when
-//        CommentResponse result = commentExternalService.createComment(createCommentRequest, 2L, 1L);
-//
-//        // then
-//        assertThat(result).isNotNull();
-//        assertThat(result.id()).isEqualTo(savedComment.getId());
-//        assertThat(result.taskId()).isEqualTo(task.getId());
-//        assertThat(result.userId()).isEqualTo(user.getId());
-//        assertThat(result.user()).isEqualTo(user);
-//        assertThat(result.content()).isEqualTo(createCommentRequest.content());
-//        assertThat(result.parentId()).isNull();
-//    }
-//
-//    @Test
-//    @DisplayName("[CommentService - updateComment] 정상 작동 확인")
-//    @Transactional
-//    void updateComment() {
-//
-//        // given
-//        CreateCommentRequest createCommentRequest = new CreateCommentRequest("내용", null);
-//        User user = new User();
-//        Task task = new Task("title", "description", user, user, TaskStatus.TODO, TaskPriority.HIGH, LocalDateTime.now());
-//        ReflectionTestUtils.setField(user, "id", 1L);
-//        ReflectionTestUtils.setField(task, "id", 2L);
-//        Comment comment = Comment.create(createCommentRequest, task, user);
-//
-//        UpdateCommentRequest updateCommentRequest = new UpdateCommentRequest("업데이트");
-//        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
-//
-//        // when
-//        CommentResponse result = commentExternalService.updateComment(updateCommentRequest, 1L);
-//
-//        // then
-//        assertThat(result).isNotNull();
-//        assertThat(result.id()).isEqualTo(comment.getId());
-//        assertThat(result.taskId()).isEqualTo(task.getId());
-//        assertThat(result.userId()).isEqualTo(user.getId());
-//        assertThat(result.user()).isEqualTo(user);
-//        assertThat(result.content()).isEqualTo(updateCommentRequest.content());
-//        assertThat(result.parentId()).isNull();
-//    }
+    @InjectMocks
+    private CommentExternalService commentExternalService;
+
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Test
+    @DisplayName("[CommentService - createdComment] 정상 작동 확인")
+    @Transactional
+    void createComment() {
+
+        // given
+        CreateCommentRequest createCommentRequest = new CreateCommentRequest("내용", null);
+        User user = new User();
+        Task task = new Task("title", "description", user, user, TaskStatus.TODO, TaskPriority.HIGH, LocalDateTime.now());
+        ReflectionTestUtils.setField(user, "id", 1L);
+        ReflectionTestUtils.setField(task, "id", 2L);
+        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task));
+        given(userRepository.findById(anyLong())).willReturn(Optional.of(user));
+
+        Comment savedComment = Comment.create(createCommentRequest, task, user);
+        ReflectionTestUtils.setField(savedComment, "id", 1L);
+        given(commentRepository.save(any(Comment.class))).willReturn(savedComment);
+
+        // when
+        CommentResponse result = commentExternalService.createComment(createCommentRequest, 2L, 1L);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.id()).isEqualTo(savedComment.getId());
+        assertThat(result.taskId()).isEqualTo(task.getId());
+        assertThat(result.userId()).isEqualTo(user.getId());
+        assertThat(result.user()).isEqualTo(user);
+        assertThat(result.content()).isEqualTo(createCommentRequest.content());
+        assertThat(result.parentId()).isNull();
+    }
+
+    @Test
+    @DisplayName("[CommentService - updateComment] 정상 작동 확인")
+    @Transactional
+    void updateComment() {
+
+        // given
+        CreateCommentRequest createCommentRequest = new CreateCommentRequest("내용", null);
+        User user = new User();
+        Task task = new Task("title", "description", user, user, TaskStatus.TODO, TaskPriority.HIGH, LocalDateTime.now());
+        ReflectionTestUtils.setField(user, "id", 1L);
+        ReflectionTestUtils.setField(task, "id", 2L);
+        Comment comment = Comment.create(createCommentRequest, task, user);
+
+        UpdateCommentRequest updateCommentRequest = new UpdateCommentRequest("업데이트");
+        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+
+        // when
+        CommentResponse result = commentExternalService.updateComment(updateCommentRequest, 1L);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.id()).isEqualTo(comment.getId());
+        assertThat(result.taskId()).isEqualTo(task.getId());
+        assertThat(result.userId()).isEqualTo(user.getId());
+        assertThat(result.user()).isEqualTo(user);
+        assertThat(result.content()).isEqualTo(updateCommentRequest.content());
+        assertThat(result.parentId()).isNull();
+    }
 }
